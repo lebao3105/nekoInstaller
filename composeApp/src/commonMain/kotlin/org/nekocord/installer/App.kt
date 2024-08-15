415fbedb36ef7d978f31e6a64a7fa4e97b2cd798
lebao3105 <bao12345yocoo@gmail.com>
2024-08-15 16:24:36 +0700

    Update UI

--- composeApp/src/commonMain/kotlin/org/nekocord/installer/App.kt
+++ composeApp/src/commonMain/kotlin/org/nekocord/installer/App.kt
@@ -4,26 +4,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -36,16 +32,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.nekocord.installer.resources.Res
-import org.nekocord.installer.resources.ic_launcher
-import org.nekocord.installer.resources.ic_launcher_foreground
 import org.nekocord.installer.resources.nekocord_logo_512
 import org.nekocord.installer.resources.nekocord_wordmark
 
@@ -65,7 +58,8 @@ fun App() {
                     title = {
                         Image(
                             painterResource(Res.drawable.nekocord_wordmark),
-                            null
+                            null,
+                            modifier = Modifier.scale(0.5f, 0.5f)
                         )
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
@@ -85,11 +79,12 @@ fun App() {
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+                modifier = Modifier.fillMaxWidth()//.fillMaxHeight()
             ) {
                 Image(
                     painterResource(Res.drawable.nekocord_logo_512),
                     null,
+                    modifier = Modifier.scale(0.5f, 0.5f)
                 )
 
                 Text(greeting.greet(), textAlign = TextAlign.Center)
@@ -98,6 +93,8 @@ fun App() {
                     "Discord is installed in ${greeting.getDiscordPath()}",
                     textAlign = TextAlign.Center
                 )
+
+                Spacer(modifier = Modifier.height(height = 8.dp))
 
                 Button(onClick = {}) {
                     Text("Click me!")
@@ -112,7 +109,8 @@ fun App() {
         if (showSettings) {
             ModalBottomSheet(
                 onDismissRequest = { showSettings = false },
-                sheetState = sheetState
+                sheetState = sheetState,
+                modifier = Modifier.fillMaxHeight()
             ) {
                 Column(
                     verticalArrangement = Arrangement.Center,
@@ -130,7 +128,10 @@ fun App() {
                             "Discord branch: ${greeting.getDiscordBranchAsString()}",
                             modifier = Modifier.align(Alignment.CenterVertically)
                         )
-                        IconButton(onClick = { comboboxExpanded = true }) {
+                        IconButton(
+                            onClick = { comboboxExpanded = true },
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        ) {
                             Icon(Icons.Rounded.Edit, null)
                         }
                         DropdownMenu(comboboxExpanded, { comboboxExpanded = false }) {
